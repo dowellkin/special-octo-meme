e72c3c0c5866b6db48563d17d9bbaa2ad415286f
Alexandr Korolev <dowellkin@gmail.com>
2023-04-19 16:35:08 +0300

    some optimization

--- canvasDrawer/main.js
+++ canvasDrawer/main.js
@@ -1,24 +1,28 @@
 const canvases = document.querySelectorAll(".section__bg canvas:not(.temp-canvas)");
 const gap = 30;
-const quality = 15;
-const animationSpeed = 2;
+let quality = 15;
+const animationSpeed = 1.5;
 const contexts = [];
 
+let someData = {
+    imageAlpha: 0,
+};
+let dataPrev = Date.now();
 
 let img = new window.Image();
 img.crossOrigin = `Anonymous`;
 
-img.src = "./assets/test.png";
+img.src = "./assets/logo-my-biz-15-darker.jpg";
 
 img.onload = function() {
-    // const tempCanvas = document.createElement("canvas");
-    const tempCanvas = document.querySelector(".temp-canvas");
+    const tempCanvas = document.createElement("canvas");
+    // const tempCanvas = document.querySelector(".temp-canvas");
     // tempCanvas.width = img.width;
     // tempCanvas.height = img.height;
     tempCanvas.width = canvases[0].width;
     tempCanvas.height = canvases[0].height;
 
-    const tempCtx = tempCanvas.getContext("2d", { willReadFrequently: true });
+    const tempCtx = tempCanvas.getContext("2d", { willReadFrequently: true, alpha: false });
     const left = (tempCanvas.width - img.width) / 2;
     const top = (tempCanvas.height - img.height) / 2;
     // tempCtx.drawImage(img, left, top);
@@ -28,14 +32,18 @@ img.onload = function() {
     let data = imgData.data;
 
     let part = 0;
-    let dt = 0.01;
+    let dt = 0.1;
     function makeImageDarker(alpha = 1) {
         tempCtx.save();
-        tempCtx.fillStyle = "rgba(0, 0, 0, 1)";
-        tempCtx.fillRect(0, 0, canvases[0].width, canvases[0].height);
-        tempCtx.globalAlpha = alpha;
-        // tempCtx.drawImage(img, left, top);
+        // tempCtx.fillStyle = "rgba(0, 0, 0, 1)";
+        // tempCtx.fillRect(0, 0, canvases[0].width, canvases[0].height);
+        tempCtx.canvas.width = tempCtx.canvas.width;
         drawImageScaled(img, tempCtx);
+        
+        tempCtx.fillStyle = `rgba(0, 0, 0, ${1 - alpha})`;
+        tempCtx.fillRect(0, 0, canvases[0].width, canvases[0].height);
+        // tempCtx.globalAlpha = alpha;
+        // tempCtx.drawImage(img, left, top);
         tempCtx.restore();
 
         part += 1;
@@ -66,29 +74,64 @@ img.onload = function() {
         }
         window.requestAnimationFrame(animateFunction);
     }
+
+    TweenLite.to(someData, .5, { imageAlpha: 1, ease: "power2.out" });
     startAnimaton(part);
+
+
+    
+    (window.onresize = () => {
+        canvases.forEach(canvas => {
+            canvas.width = innerWidth, canvas.height = innerHeight;
+        });
+        tempCanvas.width = innerWidth, tempCanvas.height = innerHeight;
+        drawImageScaled(img, tempCtx);
+        imgData = tempCtx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+        data = imgData.data;
+
+        if(innerWidth < 1024) {
+            quality = 10
+        }
+
+        if(innerWidth < 850) {
+            quality = 5
+        }
+    })();
 }
 
 for (let canvasEl of canvases) {
     canvasEl.width = canvasEl.clientWidth;
     canvasEl.height = canvasEl.clientHeight;
-    const ctx = canvasEl.getContext("2d");
+    const ctx = canvasEl.getContext("2d", { alpha: false });
     contexts.push(ctx);
 
     draw(ctx, null, 1);
 }
 
+function hideImage(time = 3) {
+    TweenLite.to(someData, time, { imageAlpha: 0, ease: "power2.out" });
+}
+
+function showImage(time = 3) {
+    TweenLite.to(someData, time, { imageAlpha: 1, ease: "power2.out" });
+}
+
+document.querySelector(".ctrl-btn--hide").addEventListener('click', () => hideImage());
+document.querySelector(".ctrl-btn--show").addEventListener('click', () => showImage());
+
 function draw(ctx, imageData, perlinOffset) {
     ctx.beginPath();
-    ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = "#000";
-    ctx.fill();
+    // ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // ctx.fillStyle = "#000";
+    // ctx.fill();
+    ctx.canvas.width = ctx.canvas.width;
 
     let local_gap = gap;
     if(local_gap < 1) local_gap = 20;
-
+        
+    ctx.beginPath();
     for (let y = gap; y < ctx.canvas.height; y += local_gap) {
-        ctx.beginPath();
+        
         const prln = perlin.get(perlinOffset / 200, (y + perlinOffset / 2) / 200);
         let prln_val = map_range(prln, 0, 1, 0, 30);
         ctx.moveTo(0, y - prln_val);
@@ -97,19 +140,25 @@ function draw(ctx, imageData, perlinOffset) {
                 if(imageData) {
                     const pixelStart = (ctx.canvas.width * y + x) * 4;
                     const val = imageData[pixelStart];
-                    local_y -= map_range(val, 0, 255, 0, 80);
-                    // console.log(perlinOffset);
+                    local_y -= map_range(val, 0, 255, 0, 80) * someData.imageAlpha;
                 }
                 if(perlinOffset) {
                     const prln = perlin.get((x + perlinOffset) / 200, (y + perlinOffset / 2) / 200);
                     let prln_val = map_range(prln, 0, 1, 0, 30);
                     local_y -= prln_val;
                 }
-                ctx.lineTo(x, local_y);
+                ctx.lineTo(x, local_y << 0);
+
+                let dateNow = Date.now();
+                if(dateNow >= dataPrev + 70) {
+                    console.log(dateNow - dataPrev, x, local_y, perlinOffset);
+                }
+                dataPrev = dateNow;
             }
-        ctx.strokeStyle = "rgba(255, 255, 255, .6)";
-        ctx.stroke();
+        
     }
+    ctx.strokeStyle = "rgba(255, 255, 255, .6)";
+    ctx.stroke();
 }
 
 function map_range(value, low1, high1, low2, high2) {
